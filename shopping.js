7cbc45ea681dc0900d2fbc4a845e19ce59fc1316
Chris Johnson <csjohnson79@icloud.com>
2015-12-06 11:07:37 -0700

    FINALIZED SHOPPING LIST APP WITH MARIUS

--- shopping.js
+++ shopping.js
@@ -8,6 +8,8 @@ function addItem() {
 
 	$('.grocery-items').append(row);
 
+	itemValue = $('#add-item-value').val('');
+
 }
 
 function crossOut() {
@@ -20,13 +22,13 @@ function removeItem() {
 }
 
 function clearAll() {
-	$(.grocery-items).empty();
+	$('.grocery-items').empty();
 }
 
 
 $(document).ready(function() {
 	/* click on the add to list button, id is "add-button", the action is to add the item */
-	$('#add-button').on('click', addItem);
+	$('#add-button').on(	'click', addItem);
 	/* click on the remove all button, class is "remove-all", the action is to Clear the entire List  */
 	$('.remove-all').on('click', clearAll);
 	
@@ -35,5 +37,13 @@ $(document).ready(function() {
 	/* click on the check box,  class is "checkbox", the action is to make a checkmark and crossout the line */
 $(document).on('click', '.checkbox', crossOut);
 
+
 	/* click on the remove button,  class is "remove", the action is to remove the item completely*/
 $(document).on('click', '.remove', removeItem);	
+
+$(document).on('keypress', function (key) {
+    //keyCode == 13 is the ENTER key
+    if (key.keyCode == 13) {
+        addItem();
+    }
+});
